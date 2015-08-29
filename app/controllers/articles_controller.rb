7eb6d5728f54c12df0fc48a1e50dd797a4b4bedd
kter <me@kter.jp>
2015-08-29 19:12:54 +0900

    エラーを出すようにした

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,17 @@
 class ArticlesController < ApplicationController
   def new
+    # エラーでnewに来た時にエラー内容が表示されるように
+    @article = Article.new
   end
 
   def create
-    @article = Article.new( article_params)
+    @article = Article.new(article_params)
 
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
   def show
